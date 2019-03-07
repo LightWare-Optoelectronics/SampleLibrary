eab2d69a5b956db4e3136d500b29b03fb5c9e3b4
Robert Gowans <rob@lightware.co.za>
2019-03-07 14:24:21 +0200

    Fixed tabs.

--- Linux SF11 C/main.cpp
+++ Linux SF11 C/main.cpp
@@ -34,16 +34,16 @@ int portConnect(const char* Name, int BitRate) {
 	cfsetispeed(&tty, BitRate);
 
 	tty.c_cflag |= (CLOCAL | CREAD);
-    tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;
-    tty.c_cflag &= ~PARENB;
-    tty.c_cflag &= ~CSTOPB;
-    tty.c_cflag &= ~CRTSCTS;
-    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
-    tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
-    tty.c_oflag &= ~OPOST;
-    tty.c_cc[VMIN] = 0;
-    tty.c_cc[VTIME] = 1;
+	tty.c_cflag &= ~CSIZE;
+	tty.c_cflag |= CS8;
+	tty.c_cflag &= ~PARENB;
+	tty.c_cflag &= ~CSTOPB;
+	tty.c_cflag &= ~CRTSCTS;
+	tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+	tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+	tty.c_oflag &= ~OPOST;
+	tty.c_cc[VMIN] = 0;
+	tty.c_cc[VTIME] = 1;
 
 	if (tcsetattr(fd, TCSANOW, &tty) != 0) {
 		printf("Error from tcsetattr\n");
